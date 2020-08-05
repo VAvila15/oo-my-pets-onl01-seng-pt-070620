2c22af14f24bf8e0b84a3b012881e7f3a0763904
Victor Avila <avilavictor1570@gmail.com>
2020-08-05 00:27:42 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -8,7 +8,7 @@ class Owner
     @name = name
     @species = "human"
     @@all << self
-    self.cats
+    @cats = []
   end
   
   def say_species
@@ -26,5 +26,8 @@ class Owner
   def self.reset_all
     @@all.clear
   end
+  
+  def add_cat
+    
 
 end
